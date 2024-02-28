d43e4cce990db56546e93897af078c6811e115fa
Elniel <corgoo7z49h@gmail.com>
2024-02-28 23:15:07 +0200

    add selection sort code

--- 6-Sorting/Code/src/com/Elniel/Main.java
+++ 6-Sorting/Code/src/com/Elniel/Main.java
@@ -5,11 +5,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {4, 5, 1, 2, 3};
-        
-        insertion(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr2 = {-4, 2, 1, 5, 0};
+//        insertion(arr);
+        selection(arr2);
+//        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
 
     }
+
+    static void selection(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1; // last unordered item index from the right
+            int maxIndex = getMaxIndex(arr, 0, last);
+
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+
 
     static void insertion(int[] arr) {
                             // i < arr.length - 2 => if i = 4 and j = 5 (out of the bound)
